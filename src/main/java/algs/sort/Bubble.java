7cf412852f8e0cfb7298c0db11451df84dbb6077
didi <xiamengseng@didiglobal.com>
2020-08-17 23:02:15 +0800

    first

---
+++ src/main/java/algs/sort/Bubble.java
@@ -0,0 +1,23 @@
+package algs.sort;
+
+public class Bubble {
+
+    public void sort(Integer[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean isSort = true;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    isSort = false;
+                }
+            }
+            if (isSort) {
+                break;
+            }
+        }
+    }
+
+}
